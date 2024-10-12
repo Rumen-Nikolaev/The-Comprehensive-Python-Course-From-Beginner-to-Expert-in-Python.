bb90d373fe697dfc0c1d91e0bda3d6d7ad5de4f4
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-12 15:52:25 +0300

    Create Tuples.py

---
+++ Chapter 3: Fundamentals of Python Objects and Data Structures/##9 Tuples with Python/Tuples.py
@@ -0,0 +1,22 @@
+t = (1,2,3)
+mylist = [1,2,3]
+print(type(t))
+print(type(mylist))
+print(len(t))
+print(t)
+
+t = ('one', 2)
+print(t[0])
+
+print(t[-1])
+
+t = ('a','a','b')
+print(t.count('a'))
+
+print(t.index('a'))
+print(t.index('b'))
+print(t)
+print(mylist)
+
+mylist[0] = 'NEW'
+print(mylist)
