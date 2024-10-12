c38adf0fcf46cf73ac89b15ae357385c7eb9a699
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-12 17:24:13 +0300

    Create while.py

---
+++ Chapter 4: Python Statements/## 3. While Loops in Python/while.py
@@ -0,0 +1,17 @@
+x = 0
+
+while x < 5:
+    print(f'The current value of x is {x}')
+    
+    x = x + 1
+else:
+    print("X IS NOT LESS THAN 5")
+    
+x = [1,2,3]
+
+mystring = 'Sammy'
+
+for letter in mystring:
+    if letter == 'a':
+        break
+    print(letter)
